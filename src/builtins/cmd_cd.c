1ed8b905a7828ca4b7ed8de897bd307f57829b84
Alexandre Teixeira <alexandremagteixeira@gmail.com>
2025-04-14 15:45:49 +0100

    added cd handle too many arguments

--- src/builtins/cmd_cd.c
+++ src/builtins/cmd_cd.c
@@ -6,7 +6,7 @@
 /*   By: paalexan <paalexan@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 16:12:15 by paalexan          #+#    #+#             */
-/*   Updated: 2025/04/10 21:47:39 by paalexan         ###   ########.fr       */
+/*   Updated: 2025/04/14 15:44:13 by paalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ int	cmd_cd(t_cmd *cmd, t_minishell *sh)
 	char	*oldpwd;
 	char	*target;
 
+	if (cmd->argv[2][0])
+		return (exit_error("minishell: cd: too many arguments\n", 1));
 	oldpwd = getcwd(NULL, 0);
 	if (!cmd->argv[1] || ft_strncmp(cmd->argv[1], "~", 2) == SUCCESS)
 		target = extract_var("HOME", sh->env);
