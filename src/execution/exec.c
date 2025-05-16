1adf49d449893fee6b09fabff5403db2fc7d8671
Alexandre Teixeira <alexandremagteixeira@gmail.com>
2025-05-16 14:22:07 +0100

    fixed still reachable block when executing invalid cmd

--- src/execution/exec.c
+++ src/execution/exec.c
@@ -6,7 +6,7 @@
 /*   By: paalexan <paalexan@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 18:31:43 by paalexan          #+#    #+#             */
-/*   Updated: 2025/05/15 19:25:08 by paalexan         ###   ########.fr       */
+/*   Updated: 2025/05/16 14:21:27 by paalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,7 +80,16 @@ static int	has_valid_cmd(t_cmd *cmd)
 	while (cmd)
 	{
 		if (cmd->argv && cmd->argv[0] && cmd->is_valid)
-			return (SUCCESS);
+		{
+			if (cmd->is_builtin)
+				return (SUCCESS);
+			else
+			{
+				ft_putstr_fd(cmd->argv[0], STDERR_FILENO);
+				ft_putstr_fd(": command not found\n", STDERR_FILENO);
+				return (FAILURE);
+			}
+		}
 		if (cmd->argv[0][0] == '\0')
 		{
 			ft_putstr_fd("Command '' not found\n", STDERR_FILENO);
